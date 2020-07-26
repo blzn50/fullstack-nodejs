17244f19089b04510be868caf6442bea60472b9d
naren <blazeraze.naren@gmail.com>
2020-07-26 21:33:07 +0545

    added body-parser and styling from static folder

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const shopRoutes = require('./routes/shop');
 const adminRoutes = require('./routes/admin');
+
 const app = express();
 
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(morgan('tiny'));
 
 app.use(shopRoutes);

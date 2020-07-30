f56f73a47f19d6ddb5dc8ddf9b40505a97b335a3
naren <blazeraze.naren@gmail.com>
2020-07-30 19:38:09 +0545

    postgresql db connection setup

---
+++ utils/db.js
@@ -0,0 +1,20 @@
+const { Pool } = require('pg');
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'store',
+  password: 'admin',
+  port: 5432,
+});
+
+module.exports = {
+  query: (text, params) => {
+    return pool.query(text, params);
+  },
+  getClient: () => {
+    return pool.connect();
+  },
+};
